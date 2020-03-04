2b1be0832dda003e902c3d4d76f3d9a9781a6f68
Aleksander <Zakharov.owl@gmail.com>
2020-03-04 22:23:15 +0300

    feat: add task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let a = n * (-1);
+    let result = n.toString().split('').reverse().join('');
+    if(n<0) {
+        return a.toString().split('').reverse().join('');
+    }
+  return result;
 }
